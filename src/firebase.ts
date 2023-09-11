8d9f37ab45e361fa9457bd83a78286a3e22da393
Alessandro Navarro Ishizawa <a.navarro.ishizawa@gmail.com>
2023-09-11 12:46:02 -0300

    feat: firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,8 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDGN0KDLGoPSS6jT5EIrHOGCq1Jk5VHg1s",
@@ -11,5 +14,9 @@ const firebaseConfig = {
   measurementId: "G-VPN9C1PE48"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export const db = getFirestore(app)
+export const auth = getAuth(app)
