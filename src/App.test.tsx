b89147abe55b3de1574bb5c145a45c20b3c649b7
Alessandro Navarro Ishizawa <a.navarro.ishizawa@gmail.com>
2023-09-11 15:21:03 -0300

    test: app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const TestComponent = () => <div>Testing</div>;
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <TestComponent />,
+      },
+    ],
+  },
+]);
+
+const renderApp = () => {
+  render(<RouterProvider router={router} />);
+};
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  renderApp();
+  const loginButton = screen.getByText('Login');
+  expect(loginButton).toBeInTheDocument();
+  expect(screen.getByText('Testing')).toBeInTheDocument();
 });
